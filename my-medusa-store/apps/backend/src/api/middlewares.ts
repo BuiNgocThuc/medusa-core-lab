4ee13931e0f8836cd54a763c36e6f4d70790569c
linh-ca-nhan <hailinh2910203@gmail.com>
2026-09-24 13:29:53 +0700

    feat: add API middleware configurations for admin tiers and customer loyalty points

--- my-medusa-store/apps/backend/src/api/middlewares.ts
+++ my-medusa-store/apps/backend/src/api/middlewares.ts
@@ -11,42 +11,113 @@ import { CreateTierSchema, UpdateTierSchema } from "@/src/api/admin/tiers";
 import { RedeemLoyaltyPointsSchema } from "@/src/api/store/carts/[id]/loyalty-points/validators.ts";
 
 export default defineMiddlewares({
-    routes: [
-        {
-            matcher: "/store/carts",
-            middlewares: [allowFields("custom", "custom.custom_name"),],
 
+    routes: [
+        // create tier
+        {
+            matcher: "/admin/tiers",
+            methods: ["POST"],
+            middlewares: [validateAndTransformBody(CreateTierSchema)],
         },
+        // retrieve all tiers
+        {
+            matcher: "/admin/tiers",
+            methods: ["GET"],
+            middlewares: [
+                validateAndTransformQuery(createFindParams(), {
+                    isList: true,
+                    defaults: ["id", "name", "promotion.id", "promotion.code"],
+                }),
+            ],
+        },
+        // retrieve tier by id
+        {
+            matcher: "/admin/tiers/:id",
+            methods: ["GET"],
+            middlewares: [
+                validateAndTransformQuery(createFindParams(), {
+                    isList: false,
+                    defaults: ["id", "name", "promotion.id", "promotion.code", "tier_rules.*"],
+                }),
+            ],
+        },
+        // update tier
+        {
+            matcher: "/admin/tiers/:id",
+            methods: ["POST"],
+            middlewares: [validateAndTransformBody(UpdateTierSchema)],
+        },
+        // retrieve customers in tier
+        {
+            matcher: "/admin/tiers/:id/customers",
+            methods: ["GET"],
+            middlewares: [
+                validateAndTransformQuery(createFindParams(), {
+                    isList: true,
+                    defaults: ["id", "email", "first_name", "last_name"],
+                }),
+            ],
+        },
+        // retrieve next tier
+        {
+            matcher: "/store/customers/me/next-tier",
+            methods: ["GET"],
+            middlewares: [
+                authenticate("customer", ["session", "bearer"]),
+                validateAndTransformQuery(NextTierSchema, {}),
+            ],
+        },
+        {
+            matcher: "/store/customers/me/loyalty-points",
+            methods: ["GET"],
+            middlewares: [authenticate("customer", ["session", "bearer"])],
+        },
+        {
+            matcher: "/store/customers/me/cart",
+            methods: ["GET"],
+            middlewares: [authenticate("customer", ["session", "bearer"])],
+        },
+        // get loyalty points
+        {
+            matcher: "/store/carts/:id/loyalty-points",
+            methods: ["POST"],
+            middlewares: [
+                authenticate("customer", ["session", "bearer"]),
+                validateAndTransformBody(RedeemLoyaltyPointsSchema),
+            ],
+        },
+        {
+            matcher: "/store/carts/:id/loyalty-points",
+            methods: ["DELETE"],
+            middlewares: [authenticate("customer", ["session", "bearer"])],
+        },
+        //...
         {
             matcher: "/store/carts/:id/merge-customer",
             method: "POST",
             middlewares: [
                 authenticate("customer", ["session", "bearer"]),
-                validateAndTransformQuery(
-                    createSelectParams(),
-                    {
-                        defaults: [ // có thể giúp khi đi vào route.ts dùng fields: req.queryConfig.fields, // <-- Tự động lấy các fields từ defaults hoặc từ ?fields của client
-                            "id",
-                            "email",
-                            "customer_id",
-                            "items.*",
-                            "region.*",
-                            "shipping_address.*",
-                        ],
-                        isList: false, // trả về 1, tránh các logic phân trang k cần thiết
-                    }
-                ),
+                validateAndTransformQuery(createSelectParams(), {
+                    defaults: [
+                        "id",
+                        "email",
+                        "customer_id",
+                        "items.*",
+                        "region.*",
+                        "shipping_address.*",
+                    ],
+                    isList: false,
+                }),
             ],
         },
         {
             matcher: "/store/carts/restore-customer",
             method: "POST",
-            middlewares: [
-                authenticate("customer", ["session", "bearer"]),
-            ],
+            middlewares: [authenticate("customer", ["session", "bearer"])],
         },
-    ]
-})
+        {
+            matcher: "/store/carts",
+            middlewares: [allowFields("custom", "custom.custom_name"),],
 
         },
         //..
